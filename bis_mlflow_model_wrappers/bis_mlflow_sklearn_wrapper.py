1bc44122be792b65418e5bde48472a540fab3b24
DhruvBhugwan <Dhruv.Bugwan@wits.ac.za>
2022-09-19 12:03:08 +0200

    Added classifier and regression wrapper

--- bis_mlflow_model_wrappers/bis_mlflow_sklearn_wrapper.py
+++ bis_mlflow_model_wrappers/bis_mlflow_sklearn_wrapper.py
@@ -9,7 +9,7 @@ sklearn_model_path = "model.pkl"
 sklearn_artifacts = {"sklearn_model": sklearn_model_path}
 
 # create wrapper
-class SKLearnWrapper(mlflow.pyfunc.PythonModel):
+class SKLearnClassifierWrapper(mlflow.pyfunc.PythonModel):
     def __init__(self):
         pass
 
@@ -20,3 +20,14 @@ class SKLearnWrapper(mlflow.pyfunc.PythonModel):
         return self.sklearn_model.predict(data), self.sklearn_model.predict_proba(data)
         # model_predictions = self.sklearn_model.predict_proba(data)
         # return [[np.max(row), np.argmax(row)] for row in model_predictions]
+
+
+class SKLearnRegressionWrapper(mlflow.pyfunc.PythonModel):
+    def __init__(self):
+        pass
+
+    def load_context(self, context):
+        self.sklearn_model = pickle.load(open(context.artifacts["sklearn_model"], "rb"))
+
+    def predict(self, model, data):
+        return self.sklearn_model.predict(data)
