062816b13523e9f6a18ee1b896a1b87277c114ca
DhruvBhugwan <Dhruv.Bugwan@wits.ac.za>
2022-09-19 10:50:23 +0200

    changed sklearn classifier return type

--- bis_mlflow_model_wrappers/bis_mlflow_sklearn_wrapper.py
+++ bis_mlflow_model_wrappers/bis_mlflow_sklearn_wrapper.py
@@ -17,6 +17,6 @@ class SKLearnWrapper(mlflow.pyfunc.PythonModel):
         self.sklearn_model = pickle.load(open(context.artifacts["sklearn_model"], "rb"))
 
     def predict(self, model, data):
-        return self.sklearn_model.predict_proba(data)
+        return self.sklearn_model.predict(data), self.sklearn_model.predict_proba(data)
         # model_predictions = self.sklearn_model.predict_proba(data)
         # return [[np.max(row), np.argmax(row)] for row in model_predictions]
