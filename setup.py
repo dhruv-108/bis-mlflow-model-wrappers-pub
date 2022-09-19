214394618c2a2e6d1a4046be55f254ab6a32711e
DhruvBhugwan <Dhruv.Bugwan@wits.ac.za>
2022-09-19 12:04:36 +0200

    Updated build

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="bis_mlflow_model_wrappers",
     packages=find_packages(include=["bis_mlflow_model_wrappers"]),
-    version="0.1.10",
+    version="0.1.11",
     description="WITS BIS Mlflow model wrappers",
     author="Dhruv Bhugwan",
     license="MIT",
